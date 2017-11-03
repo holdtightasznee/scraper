507b1e16531b85993d0c049af242d0fb0ebb11da
Marshall McDougall <marshall.mcdougall@gmail.com>
2017-11-02 19:37:47 -0700

    Cleaning of scraper.py Nov 2

--- scraper.py
+++ scraper.py
@@ -9,19 +9,47 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from bin import Bin
-import urllib2
+#import urllib2
 from bs4 import UnicodeDammit
 from contextlib import closing
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 import time
+from fastkml import kml
 
+k = kml.KML()
+ns = '{http://www.opengis.net/kml/2.2}'
+# Create a KML Document and add it to the KML root object
+d = kml.Document(ns, 'docid', 'doc name', 'doc description')
+k.append(d)
 
+# Create a KML Folder and add it to the Document
+f = kml.Folder(ns, 'fid', 'f name', 'f description')
+d.append(f)
+
+# Create a KML Folder and nest it in the first Folder
+nf = kml.Folder(ns, 'nested-fid', 'nested f name', 'nested f description')
+f.append(nf)
+
+# Create a second KML Folder within the Document
+f2 = kml.Folder(ns, 'id2', 'name2', 'description2')
+d.append(f2)
+
+# Create a Placemark with a simple polygon geometry and add it to the
+# second folder of the Document
+p = kml.Placemark(ns, 'id', 'name', 'description')
+f2.append(p)
+
+# Print out the KML Object as a string
+print(k.to_string(prettyprint=True))
+
+#Google API information
 googleApiKey = "AIzaSyCr90UZMD3fl6SLE7cHIDvWF4yofQdWIgk"
 gmaps = googlemaps.Client(key=googleApiKey)
 
 
-#Salvation Army Thrift Store
+#Salvation Army Thrift Store - done
+print("Salvation Army")
 url1 = "https://www.thriftstore.ca/british-columbia/drop-bin-locations"
 r1 = requests.get(url1)
 
@@ -47,9 +75,8 @@ for td in filtered1:
 		count += 1
 	list.append(addBin)
 
-
-
 #Inclusion BC & CPABC Clothing
+print("Inclusion BC and CPABC Clothing")
 url2 = "http://www.google.com/maps/d/kml?mid=1kqVfqYiPtnqrO8L5zC_yVkAiwB0&forcekml=1"
 url3 = "https://www.google.com/maps/d/kml?mid=1ekVOoKgoAW2vIjiDh3DmP4OU04g&forcekml=1"
 r2 = requests.get(url2)
@@ -61,16 +88,20 @@ soup3 = BeautifulSoup(r3.content,"lxml")
 filtered2 = soup2.find_all("placemark")
 filtered3 = soup3.find_all("placemark")
 
+print("Inclusion BC")
 for item in filtered2:
 	addBin = Bin("", "", "", "", "", "")
 	addBin.address = item.find_all("name")[0].text.strip()
 	addBin.coordinate = item.find_all("coordinates")[0].text.strip()
+	addBin.company = "Inclusion BC"
 	list.append(addBin)
 
-for item in filtered2:
+print("Cerebral Palsy Association of British Columbia")
+for item in filtered3:
 	addBin = Bin("", "", "", "", "", "")
 	addBin.address = item.find_all("name")[0].text.strip()
 	addBin.coordinate = item.find_all("coordinates")[0].text.strip()
+	addBin.company = "Cerebral Palsy Association of British Columbia"
 	list.append(addBin)
 
 
@@ -78,9 +109,8 @@ for item in list:
 	if item.address:
 		list.remove(item)
 
-
-
 #Diabetes Canada
+print("Diabetes Canada")
 url1 = "http://www.diabetes.ca/dropBoxes/phpsqlsearch_genxml.php?&lat=49.2057&lng=-122.9110&radius=50"
 r1 = requests.get(url1)
 
@@ -89,10 +119,34 @@ soup1  = BeautifulSoup(r1.content,"lxml")
 filtered = soup1.find_all("marker",{"type":"Drop Box"})
 
 for item in filtered:
-    print(item['address'])
+	addBin = Bin("", "", "", "", "", "")
+	addBin.address = item['address']
+	addBin.company = "Diabetes Canada"
+	list.append(addBin)
+
+
+
+'''#Big Brothers
+print("Big Brothers")
+url = "https://www.bigbrothersvancouver.com/clothing-donation/donation-bins/"
+browser = webdriver.PhantomJS()
+browser.get(url)
+time.sleep(1)
+html = browser.page_source
+soup = BeautifulSoup(html, 'lxml')
+filtered1 = soup.find_all("span", {"class":"slp_result_address"})
+for item in filtered1:
+	print(item['class'])
+	print(item.text.strip())
+browser.close()'''
+
+'''for item in list:
+	print("-----------------------")
+	print(item)
+	print("-----------------------")'''
 
 #Develop BC
-
+'''print("develop bc")
 url = "http://www.develop.bc.ca/donate/"
 with closing(PhantomJS()) as browser:
    browser.get(url)
@@ -113,11 +167,11 @@ for result in individual_results:
        if('slp_result_citystatezip' in address_component.get("class")):
            address += ", "
 
-print(address)
+print(address)'''
   
 
 #geoencoding
-'''for item in list:
+for item in list:
 	if item.getCoordinate():
 		coordinateRaw = item.getCoordinate()
 		print(coordinateRaw)
@@ -130,15 +184,8 @@ print(address)
 		address = item.getAddress()
 		geocode_result = gmaps.geocode(address)
 		item.coordinate = reverse_geocode_result[0]['geometry']['location']
-		print(geocode_result[0]['formatted_address'])'''
+		print(geocode_result[0]['formatted_address'])
 
 
 
 
-
-
-#print all bin objects
-for item in list:
-	print("-----------------------")
-	print(item)
-	print("-----------------------")
