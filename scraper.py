7741b9492ec1abf3d58e9c97bcc8ad3c927fbf97
Marshall McDougall <marshall.mcdougall@gmail.com>
2017-11-02 17:17:00 -0700

    Work from Thursday October 26

--- scraper.py
+++ scraper.py
@@ -9,6 +9,12 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from bin import Bin
+import urllib2
+from bs4 import UnicodeDammit
+from contextlib import closing
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+import time
 
 
 googleApiKey = "AIzaSyCr90UZMD3fl6SLE7cHIDvWF4yofQdWIgk"
@@ -85,10 +91,33 @@ filtered = soup1.find_all("marker",{"type":"Drop Box"})
 for item in filtered:
     print(item['address'])
 
+#Develop BC
 
+url = "http://www.develop.bc.ca/donate/"
+with closing(PhantomJS()) as browser:
+   browser.get(url)
+   html = browser.page_source
+#r = urllib2.urlopen("http://www.bcchauxiliary.com/our-businesses/clothing-donation-bin-program/")
+#html = r.read()
+
+gmaps = googlemaps.Client(key="AIzaSyDqGPoS9GUio0FZndRTdnvNDFIatMHGeus")
+
+soup = BeautifulSoup(html, "lxml")
+individual_results = soup.find_all("div", {"class":"results_wrapper"})
+
+for result in individual_results:
+   address = ""
+   address_components = result.find_all("span", {"class":"slp_result_address"})
+   for address_component in address_components:
+       address += address_component.get_text()
+       if('slp_result_citystatezip' in address_component.get("class")):
+           address += ", "
+
+print(address)
+  
 
 #geoencoding
-for item in list:
+'''for item in list:
 	if item.getCoordinate():
 		coordinateRaw = item.getCoordinate()
 		print(coordinateRaw)
@@ -101,7 +130,11 @@ for item in list:
 		address = item.getAddress()
 		geocode_result = gmaps.geocode(address)
 		item.coordinate = reverse_geocode_result[0]['geometry']['location']
-		print(geocode_result[0]['formatted_address'])
+		print(geocode_result[0]['formatted_address'])'''
+
+
+
+
 
 
 #print all bin objects
--- test.py
+++ test.py
@@ -3,26 +3,47 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from bin import Bin
+from bs4 import UnicodeDammit
+from contextlib import closing
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+import time
 
-#url1 = "http://www.diabetes.ca/dropBoxes/phpsqlsearch_genxml.php?&lat=49.2057&lng=-122.9110&radius=50"
-#r1 = requests.get(url1)
-
-#soup1  = BeautifulSoup(r1.content,"lxml")
-
-#filtered = soup1.find_all("marker",{"type":"Drop Box"})
-
-#for item in filtered:
-#    print(item['address'])
 
 #Big Brothers
-url2 = "https://www.bigbrothersvancouver.com/clothing-donation/donation-bins/"
-r2 = requests.get(url2)
+'''
+url = "https://www.bigbrothersvancouver.com/clothing-donation/donation-bins/"
+browser = webdriver.PhantomJS()
+browser.get(url)
+time.sleep(1)
+html = browser.page_source
+soup = BeautifulSoup(html, 'lxml')
+filtered1 = soup.find_all("span", {"class":"slp_result_address"})
+for item in filtered1:
+	print(item)
+browser.close()'''
 
-soup2  = BeautifulSoup(r2.content,"lxml")
 
-filtered2 = soup2.find_all("span")
+url = "http://www.develop.bc.ca/donate/"
+browser = webdriver.PhantomJS()
+browser.get(url)
+time.sleep(1)
+html = browser.page_source
+#r = urllib2.urlopen("http://www.bcchauxiliary.com/our-businesses/clothing-donation-bin-program/")
+#html = r.read()
 
-print(filtered2)
+gmaps = googlemaps.Client(key="AIzaSyDqGPoS9GUio0FZndRTdnvNDFIatMHGeus")
 
-#for item in filtered2:
-#   print(item['address'])
+soup = BeautifulSoup(html, "lxml")
+individual_results = soup.find_all("div", {"class":"results_wrapper"})
+
+for result in individual_results:
+   address = ""
+   address_components = result.find_all("span", {"class":"slp_result_address"})
+   for address_component in address_components:
+       address += address_component.get_text()
+       if('slp_result_citystatezip' in address_component.get("class")):
+           address += ", "
+   print(address)
+
+browser.close()
