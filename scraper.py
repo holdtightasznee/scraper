2cd5e7b8dc0842b3670ceb1fb3364ac9369287c1
Jonathan Lew <lewkewarm@outlook.com>
2017-11-16 22:22:20 -0800

    added try except for most url requests

--- scraper.py
+++ scraper.py
@@ -50,60 +50,68 @@ gmaps = googlemaps.Client(key=googleApiKey)
 list = []
 
 #Salvation Army Thrift Store - done
-print("Salvation Army")
-url1 = "https://www.thriftstore.ca/british-columbia/drop-bin-locations"
-r1 = requests.get(url1)
-print("Salvation army request")
-soup1 = BeautifulSoup(r1.content,"lxml")
-filtered1 = soup1.find_all("tr")
+try:
+	print("Salvation Army")
+	url1 = "https://www.thriftstore.ca/british-columbia/drop-bin-locations"
+	r1 = requests.get(url1)
+	print("Salvation army request")
+	soup1 = BeautifulSoup(r1.content,"lxml")
+	filtered1 = soup1.find_all("tr")
 
-for td in filtered1:
-	inter = td.find_all("span")
-	count = 0
-	addBin = Bin("", "", "", "", "", "")
-	while (count <= 3):
-		value = inter[count].text.strip()
-		if(count == 0):
-			addBin.name = value
-		elif (count == 1):
-			addBin.company = value
-		elif (count == 2):
-			addBin.address = value
-		else:
-			addBin.city = value
-		count += 1
-	list.append(addBin)
-
+	for td in filtered1:
+		inter = td.find_all("span")
+		count = 0
+		addBin = Bin("", "", "", "", "", "")
+		while (count <= 3):
+			value = inter[count].text.strip()
+			if(count == 0):
+				addBin.name = value
+			elif (count == 1):
+				addBin.company = value
+			elif (count == 2):
+				addBin.address = value
+			else:
+				addBin.city = value
+			count += 1
+		list.append(addBin)
+except requests.exceptions.RequestException as e:
+	print (e)
 
 #WORKING
 #Inclusion BC & CPABC Clothing
-print("Inclusion BC and CPABC Clothing")
-url2 = "http://www.google.com/maps/d/kml?mid=1kqVfqYiPtnqrO8L5zC_yVkAiwB0&forcekml=1"
-url3 = "https://www.google.com/maps/d/kml?mid=1ekVOoKgoAW2vIjiDh3DmP4OU04g&forcekml=1"
-r2 = requests.get(url2)
-r3 = requests.get(url3)
 
-soup2 = BeautifulSoup(r2.content,"lxml")
-soup3 = BeautifulSoup(r3.content,"lxml")
+#InclusionBC
+try:
+	url2 = "http://www.google.com/maps/d/kml?mid=1kqVfqYiPtnqrO8L5zC_yVkAiwB0&forcekml=1"
+	r2 = requests.get(url2)
+	soup2 = BeautifulSoup(r2.content,"lxml")
+	filtered2 = soup2.find_all("placemark")
+	print("Inclusion BC")
+	for item in filtered2:
+		addBin = Bin("", "", "", "", "", "")
+		addBin.address = item.find_all("name")[0].text.strip()
+		addBin.coordinate = item.find_all("coordinates")[0].text.strip()
+		addBin.company = "Inclusion BC"
+		list.append(addBin)
+except requests.exceptions.RequestException as e:
+	print (e)
 
-filtered2 = soup2.find_all("placemark")
-filtered3 = soup3.find_all("placemark")
 
-print("Inclusion BC")
-for item in filtered2:
-	addBin = Bin("", "", "", "", "", "")
-	addBin.address = item.find_all("name")[0].text.strip()
-	addBin.coordinate = item.find_all("coordinates")[0].text.strip()
-	addBin.company = "Inclusion BC"
-	list.append(addBin)
-
-print("Cerebral Palsy Association of British Columbia")
-for item in filtered3:
-	addBin = Bin("", "", "", "", "", "")
-	addBin.address = item.find_all("name")[0].text.strip()
-	addBin.coordinate = item.find_all("coordinates")[0].text.strip()
-	addBin.company = "Cerebral Palsy Association of British Columbia"
-	list.append(addBin)
+#InclusionBC
+try:
+	url3 = "https://www.google.com/maps/d/kml?mid=1ekVOoKgoAW2vIjiDh3DmP4OU04g&forcekml=1"
+	r3 = requests.get(url3)
+	soup3 = BeautifulSoup(r3.content,"lxml")
+	filtered3 = soup3.find_all("placemark")
+	print("Cerebral Palsy Association of British Columbia")
+	for item in filtered3:
+		addBin = Bin("", "", "", "", "", "")
+		addBin.address = item.find_all("name")[0].text.strip()
+		addBin.coordinate = item.find_all("coordinates")[0].text.strip()
+		addBin.company = "Cerebral Palsy Association of British Columbia"
+		list.append(addBin)
+except requests.exceptions.RequestException as e:
+	print (e)
 
 
 for item in list:
@@ -112,19 +120,23 @@ for item in list:
 
 #WORKING
 #Diabetes Canada
-print("Diabetes Canada")
-url1 = "http://www.diabetes.ca/dropBoxes/phpsqlsearch_genxml.php?&lat=49.2057&lng=-122.9110&radius=50"
-r1 = requests.get(url1)
+try:
+	print("Diabetes Canada")
+	url1 = "http://www.diabetes.ca/dropBoxes/phpsqlsearch_genxml.php?&lat=49.2057&lng=-122.9110&radius=50"
+	r1 = requests.get(url1)
 
-soup1  = BeautifulSoup(r1.content,"lxml")
+	soup1  = BeautifulSoup(r1.content,"lxml")
 
-filtered = soup1.find_all("marker",{"type":"Drop Box"})
+	filtered = soup1.find_all("marker",{"type":"Drop Box"})
 
-for item in filtered:
-	addBin = Bin("", "", "", "", "", "")
-	addBin.address = item['address']
-	addBin.company = "Diabetes Canada"
-	list.append(addBin)
+	for item in filtered:
+		addBin = Bin("", "", "", "", "", "")
+		addBin.address = item['address']
+		addBin.company = "Diabetes Canada"
+		list.append(addBin)
+except requests.exceptions.RequestException as e:
+	print (e)
+
 
 #WORKING
 #Big Brothers
